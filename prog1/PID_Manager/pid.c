60551c1cd1e0e218da1e1fc27f7fad27e45151fb
Riley Kopp <koppril7@gmail.com>
2020-03-02 17:39:29 -0700

    Added aquire and release pid functions

--- prog1/PID_Manager/pid.c
+++ prog1/PID_Manager/pid.c
@@ -19,7 +19,7 @@ int allocate_map(void)
 
         for(int index = 0; index <= PID_MAX - PID_MIN; index++)
         {
-            pid_map = 0;
+            pid_map[index] = 0;
         }
 
         pthread_mutex_release(&mutex);
@@ -34,21 +34,43 @@ int allocate_map(void)
 int allocate_pid(void)
 {
     /* local variables */
+    int retval = -1;
+    int temp = -1;
 
     /* acquire the mutex lock and warn if unable */
+    //TODO warn if not released
+    pthread_mutex_lock(&mutex);
 
     /* find the next available pid */
+    for( int index = 1; index <= PID_MAX - PID_MIN && retval != -1; index++)
+    {
+        temp = pid_map[index];
+
+        if(temp == 1){
+            retval = temp;
+        }
+    }
 
     /* release and warn if the mutex was not released */
+    //TODO warn if not released
+    pthread_mutex_release(&mutex);
 
-    return last;
+    return retval;
 }
 
 // Releases a pid
 void release_pid(int pid)
 {
     /* acquire the mutex lock and warn if unable */
+    //TODO warn if not released
+    pthread_mutex_lock(&mutex);
+
+    pid_map[pid] = 0;
 
     /* release and warn if the mutex was not released  */
+    //TODO warn if not released
+    pthread_mutex_release(&mutex);
+
+    return;
 
 }
